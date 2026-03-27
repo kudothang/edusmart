be504e9557ff4f4d736185a458145dc1dc68c68f
kudothang <thangdevloper@gmail.com>
2026-03-28 00:25:37 +0700

    fix name part 2

--- src/pages/CoursePage.tsx
+++ src/pages/CoursePage.tsx
@@ -3,7 +3,7 @@ import { useCourses } from "../hooks/useCourses";
 import { useCourseFilter } from "../hooks/useCourseFilter";
 
 import { CourseFilterBar } from "../components/ui/Courses/CourseFilterBar";
-import { Pagination } from "../components/ui/Pagnito";
+import { Pagination } from "../components/ui/Pagination";
 import { usePagination } from "../hooks/usePagnition";
 import { useCourseFilterStore } from "../stores/courseFilterStore";
 import { useSearchParams } from "react-router";
